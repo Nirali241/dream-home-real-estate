dd3d4ca2c74cf27d8d27af1a9932efd0b7c0fd02
Nirali <bhalodiyanirali241@gmail.com>
2021-04-18 08:41:20 +0530

    first commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/house-5.jpg'
+import ImageTwo from '../images/house-6.jpg'
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Beautiful contructed by the expert. Very professional Business working area and good reviews.',
+    paragraphTwo: 'Browse our best-selling plans to get a sample of what we build on the production-side. You can use these plans as a starting point or as inspiration for your new home build. The sky is the limit! Let our Custom Home Expert know what you like from the library and they’ll work with you to make it your own!',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+
+}
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'ndjjsdj jnkjndjkjjk fjf dnjjfdjsdfj dnjknfjkjksfnjknk fnjknfdjknfjn ndkjnfjknjkfkjdff. ',
+    paragraphTwo: 'djsjdnfj jvnjnvjk jnvjnjnjk vnjnvjkv nvjnjkvnjnvnjkvk vnknvknvjnvv vnnvnvkvvnjknnvnjkvnknjknvj nvjknjvnkvn v knvjvnkv vvfjbvbvkvnk',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+}
